e6b49ed9e11599b59b4cd0161bc049931f6ee0e2
Niaz Baharudeen <tuk05348@temple.edu>
2024-12-24 23:15:41 -0500

    Separated printing of key into its own function. Made key_gen function return a pointer to the key instead, with the key being stored in heap memory.

--- key.c
+++ key.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include <sodium.h>
 
-void key_gen();
+unsigned char* key_gen();
+void print_key(unsigned char* key);
 
 int main(int argc, char* argv[]){
-    key_gen();
+    unsigned char* key = key_gen();
+    print_key(key);
     return 0;
 }
 
-void key_gen(){
+unsigned char* key_gen(){
     unsigned char bytes[8];
     unsigned char mask = 1;
-    unsigned char key[64];
+    unsigned char* key = malloc(64 * sizeof(unsigned char));
+    if(key == NULL){
+        perror("Memory allocation failed\n");
+    }
     randombytes_buf(bytes, 8);
     for(int i=0; i<8; i++){
         for(int j=0; j<8; j++){
@@ -20,6 +25,10 @@ void key_gen(){
         }
     }
 
+    return key;
+}
+
+void print_key(unsigned char* key){
     for(int i=0; i<64; i++){
         printf("%d", key[i]);
     }
