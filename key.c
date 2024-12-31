4fc431dd1cc17be78532cc861d6659ad4da09026
Niaz Baharudeen <tuk05348@temple.edu>
2024-12-31 15:31:44 -0500

    Changed key and subkey functions so that they take in an array rather than allocating memory themselves for the keys.

--- key.c
+++ key.c
@@ -2,35 +2,29 @@
 #include <stdlib.h>
 #include <sodium.h>
 
-unsigned char* key_gen();
+void key_gen(unsigned char* key);
 void print_key(unsigned char* key, int key_len);
-unsigned char* subkey_gen();
+void subkey_gen(unsigned char* subkey, unsigned char* key);
 
 int main(int argc, char* argv[]){
-    unsigned char* key = key_gen();
+    unsigned char key[64];
+    key_gen(key);
     print_key(key, 64);
-    unsigned char* subkey = subkey_gen(key);
+    unsigned char subkey[56];
+    subkey_gen(subkey, key);
     print_key(subkey, 56);
-    free(key);
-    free(subkey);
     return 0;
 }
 
-unsigned char* key_gen(){
+void key_gen(unsigned char* key){
     unsigned char bytes[8];
     unsigned char mask = 1;
-    unsigned char* key = malloc(64 * sizeof(unsigned char));
-    if(key == NULL){
-        perror("Memory allocation failed\n");
-    }
     randombytes_buf(bytes, 8);
     for(int i=0; i<8; i++){
         for(int j=0; j<8; j++){
             key[(i*8) + j] = (bytes[i] >> j) & mask;
         }
     }
-
-    return key;
 }
 
 void print_key(unsigned char* key, int key_len){
@@ -40,14 +34,11 @@ void print_key(unsigned char* key, int key_len){
     printf("\n");
 }
 
-unsigned char* subkey_gen(unsigned char* key){
+void subkey_gen(unsigned char* subkey, unsigned char* key){
     int PC1[56] = {57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4};
-    unsigned char *subkey = malloc(56 * sizeof(unsigned char));
-    if(subkey == NULL){
-        perror("Memory allocation failed\n");
-    }
+
     for(int i=0; i<56; i++){
         subkey[i] = key[PC1[i]];
     }
-    return subkey;
+
 }
