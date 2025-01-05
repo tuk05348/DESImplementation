8d19228452703cb9d74d49eba4d7205df76b34a2
Niaz Baharudeen <tuk05348@temple.edu>
2025-01-05 16:17:49 -0500

    Implemented the sequence of left shifts for the left and right halves of the subkey.

--- des.c
+++ des.c
@@ -6,17 +6,22 @@ int main(int argc, char *argv[]){
 	unsigned char key[64];
 	unsigned char subkey[56];
 	unsigned char cd[2][17][28];
+	int seq[] = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
 
 	key_gen(key);
 	subkey_gen(subkey, key);
 
-	for(int i=0; i<2; i++){
-		copy_arr(28, i*28, subkey, cd[i][0]);
-	}
-
 	print_key(key, 64);
 	print_key(subkey, 56);
-	print_key(cd[0][0], 28);
-	print_key(cd[1][0], 28);
+
+	for(int i=0; i<2; i++){
+		copy_arr(28, i*28, subkey, cd[i][0]);
+		printf("%d Half: ", i);
+		print_key(cd[i][0], 28);
+		for(int j=1; j<17; j++){
+			left_shift(seq[j-1], 28, cd[i][j-1], cd[i][j]);
+			print_key(cd[i][j], 28);
+		}
+	}
 	return 0;
 }
