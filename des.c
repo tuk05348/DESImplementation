493894d110e13d24f8ac888993ce1abafa9d9ca8
Niaz Baharudeen <tuk05348@temple.edu>
2025-01-05 15:42:04 -0500

    Implemented first step of des algorithm, creating the 28 bit halves from the subkey using the copy function.

--- des.c
+++ des.c
@@ -4,14 +4,19 @@
 
 int main(int argc, char *argv[]){
 	unsigned char key[64];
-	key_gen(key);
 	unsigned char subkey[56];
+	unsigned char cd[2][17][28];
+
+	key_gen(key);
 	subkey_gen(subkey, key);
+
+	for(int i=0; i<2; i++){
+		copy_arr(28, i*28, subkey, cd[i][0]);
+	}
+
 	print_key(key, 64);
 	print_key(subkey, 56);
-	unsigned char subkey_1[56];
-	left_shift(1, 56, subkey, subkey_1);
-	print_key(subkey_1, 56);
-
+	print_key(cd[0][0], 28);
+	print_key(cd[1][0], 28);
 	return 0;
 }
